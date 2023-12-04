6335af4ec53d7b43abed39169ce91f7ef06531e2
Tim Hartemink <tim.hartemink@nos.nl>
2023-12-04 09:44:26 +0100

    Day 4 - part 1

--- app/src/main/java/com/tilton/aoc2023/solution/Day4.kt
+++ app/src/main/java/com/tilton/aoc2023/solution/Day4.kt
@@ -1,8 +1,56 @@
 package com.tilton.aoc2023.solution
 
+import kotlin.math.pow
+
 class Day4 {
     fun part1(input: List<String>): Int {
-        return -1
+        val valuePerGame = mutableListOf<Int>()
+
+        input.forEach { line ->
+            val winningNumbersLine = line.substring(line.indexOf(":") + 1, line.indexOf("|"))
+            val cardNumbersLine = line.substring(line.indexOf("|") + 1)
+            val winningNumbers = getNumbers(winningNumbersLine).toSet()
+            val cardNumbers = getNumbers(cardNumbersLine).toSet()
+
+            val amountOfWinningNumbersInCard = cardNumbers.size - (cardNumbers - winningNumbers).size
+            valuePerGame.add(
+                (2.toDouble().pow(amountOfWinningNumbersInCard - 1)).toInt()
+            )
+        }
+
+        return valuePerGame.sum()
+    }
+
+    private fun getNumbers(line: String): List<Int> {
+        val numbers = mutableListOf<Int>()
+        var cachedNumber = ""
+        line.forEach { character ->
+            if (character.isDigit()) {
+                cachedNumber += character
+            } else {
+                if (storeCachedNumberIfNecessary(numbers, cachedNumber)) {
+                    cachedNumber = ""
+                }
+            }
+        }
+
+        if (storeCachedNumberIfNecessary(numbers, cachedNumber)) {
+            cachedNumber = ""
+        }
+
+        return numbers
+    }
+
+    private fun storeCachedNumberIfNecessary(
+        numbers: MutableList<Int>,
+        cachedNumber: String,
+    ): Boolean {
+        return if (cachedNumber.isNotEmpty()) {
+            numbers.add(cachedNumber.toInt())
+            true
+        } else {
+            false
+        }
     }
 
     fun part2(input: List<String>): Int {
---
+++ app/src/test/java/com/tilton/aoc2023/solution/Day4Test.kt
@@ -0,0 +1,22 @@
+package com.tilton.aoc2023.solution
+
+import com.tilton.aoc2023.TestInputLoader
+import org.junit.Test
+
+class Day4Test {
+    @Test
+    fun part1() {
+        val input = TestInputLoader.loadTestInput(4)
+        val result = Day4().part1(input)
+
+        assert(result == 13)
+    }
+
+    @Test
+    fun part2() {
+        val input = TestInputLoader.loadTestInput(4)
+        val result = Day4().part2(input)
+
+        assert(result == -1)
+    }
+}
