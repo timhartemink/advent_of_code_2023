29d8b336fbc0ced5f1dcf635e435e9f8a6471132
Tim Hartemink <tim.hartemink@nos.nl>
2023-12-07 07:41:02 +0100

    Day 7 setup

---
+++ app/src/test/java/com/tilton/aoc2023/domain/solution/days/Day7Test.kt
@@ -0,0 +1,22 @@
+package com.tilton.aoc2023.domain.solution.days
+
+import com.tilton.aoc2023.TestInputLoader
+import org.junit.Test
+
+class Day7Test {
+    private val input = TestInputLoader.loadTestInput(7)
+
+    @Test
+    fun part1() {
+        val result = Day7(input).part1()
+
+        assert(result == null)
+    }
+
+    @Test
+    fun part2() {
+        val result = Day7(input).part2()
+
+        assert(result == null)
+    }
+}
